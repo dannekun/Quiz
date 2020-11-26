46366ad7d3583ec02e949d70a6f50b75acbd5155
IvonaZoricic <ivona.zoricic@gmail.com>
2020-11-26 11:12:38 +0100

    Two ways to log in

--- src/LoginGUI.java
+++ src/LoginGUI.java
@@ -8,15 +8,15 @@ public class LoginGUI extends JFrame implements ActionListener {
     JPanel panel = new JPanel();
     JLabel user = new JLabel("Spelare");
     JTextField userText = new JTextField();
+    JButton login = new JButton("Logga in");
 
-    public LoginGUI(){
-
+    public LoginGUI() {
         add(panel);
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-        panel.setBackground( new Color(51, 133, 255));
+        panel.setBackground(new Color(51, 133, 255));
         panel.setAlignmentX(Component.CENTER_ALIGNMENT);
         panel.add(Box.createRigidArea(new Dimension(100, 80)));
-        panel.setBorder(BorderFactory.createEmptyBorder(50,50,50,50));
+        panel.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50));
         panel.add(user);
         user.setFont(new Font("Arial", Font.PLAIN, 18));
         user.setForeground(Color.WHITE);
@@ -24,20 +24,27 @@ public class LoginGUI extends JFrame implements ActionListener {
         panel.add(Box.createRigidArea(new Dimension(10, 10)));
         panel.add(userText);
         userText.setAlignmentX(Component.CENTER_ALIGNMENT);
-        userText.setPreferredSize(new Dimension(200,30));
-        userText.setMaximumSize(new Dimension(200,30));
+        panel.add(Box.createRigidArea(new Dimension(100, 15)));
+        panel.add(login);
+        login.setAlignmentX(Component.CENTER_ALIGNMENT);
+        login.setBackground(new Color(71, 71, 209));
+        login.setForeground(Color.WHITE);
+        login.setPreferredSize(new Dimension(50, 40));
+        panel.add(Box.createRigidArea(new Dimension(100, 170)));
 
         setSize(350, 500);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
         setLocationRelativeTo(null);
 
+        login.addActionListener(this);
         userText.addActionListener(this);
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == userText){
+        if (e.getSource() == userText || e.getSource() == login) {
             Player p = new Player(userText.getText());
             dispose();
             HomePage page = new HomePage(p);
